7d9928dc27c14809a122c1ffebcd12441c1a2b7a
RoyWang <lanni0619@gmail.com>
2024-12-07 12:48:34 +0800

    Modify jest and readme

--- test/book.test.js
+++ test/book.test.js
@@ -1,6 +1,8 @@
 const request = require("supertest");
 const app = require("../app");
 
+let delId;
+
 describe("REST-API of Books", () => {
   it("GET /books", async () => {
     const res = await request(app).get("/api/v1/books");
@@ -16,25 +18,25 @@ describe("REST-API of Books", () => {
     expect(res.body.data).toHaveProperty("id", postId);
   });
 
-  //   it("POST /books", async () => {
-  //     const res = await request(app).post("/api/v1/books").send({
-  //       name: "test_name",
-  //       author: "test_author",
-  //       rating: 5,
-  //       reviews: 100,
-  //       price: 10,
-  //       year: 2021,
-  //       genre: "Fiction", //enum
-  //     });
-  //     expect(res.statusCode).toEqual(201);
-  //     expect(res.body).toHaveProperty("status", "success");
-  //   });
+  it("POST /books", async () => {
+    const res = await request(app).post("/api/v1/books").send({
+      name: "test_name",
+      author: "test_author",
+      rating: 5,
+      reviews: 100,
+      price: 10,
+      year: 2021,
+      genre: "Fiction", //enum
+    });
+    delId = res.body.data.id;
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("status", "success");
+  });
 
-  //   it("DELETE /books", async () => {
-  //     const delId = 600;
-  //     const res = await request(app).delete(`/api/v1/books/${delId}`);
-  //     expect(res.statusCode).toEqual(204);
-  //   });
+  it("DELETE /books", async () => {
+    const res = await request(app).delete(`/api/v1/books/${delId}`);
+    expect(res.statusCode).toEqual(204);
+  });
 
   it("PATCH /books", async () => {
     const id = 1;
